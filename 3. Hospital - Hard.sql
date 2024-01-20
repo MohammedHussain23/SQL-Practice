f74e142fd32fbc747bd79180aeaaa9fc9a1e6d25
Hussain <hbmdhussain@gmail.com>
2024-01-20 12:52:43 +0530

    3rd hard question from Hospital DB

---
+++ 3. Hospital - Hard.sql
@@ -0,0 +1,17 @@
+/* Show patient_id, first_name, last_name, and attending doctor's specialty.
+Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
+
+Check patients, admissions, and doctors tables for required information.
+ */
+
+SELECT
+  p.patient_id,
+  p.first_name,
+  p.last_name,
+  d.specialty
+FROM patients p
+  JOIN admissions a ON p.patient_id = a.patient_id
+  JOIN doctors d ON d.doctor_id = a.attending_doctor_id
+WHERE
+  a.diagnosis = 'Epilepsy'
+  AND d.first_name = 'Lisa';
